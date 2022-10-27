96954d5e5a622ed33886e6de5925f7196b10e958
franjavi01 <fjsg0001@gmail.com>
2022-10-27 15:28:02 +0200

    creando la estructura básica con header, index y footer. Y añadiendo Bootstrap

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include 'header.php' ?>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corrupti, quibusdam quod repellendus, eum illo odio, quaerat laudantium tempora aliquid dolorem consectetur qui impedit nostrum pariatur ratione laboriosam laborum maiores?</p>
+<?php include 'footer.php' ?>
