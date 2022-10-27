0639936fdc7175121695d4928c13a5a937da3fef
franjavi01 <fjsg0001@gmail.com>
2022-10-27 18:43:35 +0200

    Añadiendo cabececera y navar en el header

--- header.php
+++ header.php
@@ -10,3 +10,63 @@
     <title>2000 películas siglo xx</title>
 </head>
 <body>
+    <img src="./assets/cabecera.jpg" class="container-fluid" alt="cabecera">
+    <nav class="navbar navbar-dark bg-primary">
+        <ul class="nav">
+            <li class="nav-item">   
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="./assets/logo.webp" alt="Logo" width="30px" height="30px" class="d-inline-block align-text-center">
+                    </a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                </a>
+                    <ul class="dropdown-menu">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
+                        </form>
+                    </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Décadas
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Años del cine mudo</a></li>
+                        <li><a class="dropdown-item" href="#">Años 30</a></li>
+                        <li><a class="dropdown-item" href="#">Años 40</a></li>
+                        <li><a class="dropdown-item" href="#">Años 50</a></li>
+                        <li><a class="dropdown-item" href="#">Años 60</a></li>
+                        <li><a class="dropdown-item" href="#">Años 70</a></li>
+                        <li><a class="dropdown-item" href="#">Años 80</a></li>
+                        <li><a class="dropdown-item" href="#">Siglo XXI</a></li>
+                    </ul>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Biografías
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Directores</a></li>
+                        <li><a class="dropdown-item" href="#">Actores</a></li>
+                        <li><a class="dropdown-item" href="#">Actrices</a></li>
+                        <li><a class="dropdown-item" href="#">Otras Biografías</a></li>
+                    </ul>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sagas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">2000 canciones siglo XX</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tienda AMAZON</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+            </li>
+        </ul>
+    </nav>
