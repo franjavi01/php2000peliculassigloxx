1525c187fdee1625d96821a55fadf92228b09d4e
franjavi01 <fjsg0001@gmail.com>
2022-10-29 18:53:08 +0200

    añadiendo el texto en el index

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php include 'header.php' ?>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse corrupti, quibusdam quod repellendus, eum illo odio, quaerat laudantium tempora aliquid dolorem consectetur qui impedit nostrum pariatur ratione laboriosam laborum maiores?</p>
+<p>Aquí tienes las mejores películas clásicas del siglo XX, para ver online y para la descarga. Si eres amante del cine clásico este es tu sitio</p>
+
+<p>El cine fue inventado por los hermanos Lumiere a finales del siglo XIX. El 28 de diciembre de 1895 proyectan su primera película en la que simplemente se ve a unos obreros saliendo de una fábrica. Los hermanos Lumìere hacen más de 500 películas sin un guión elaborado y sin la participación de actores profesionales, o sea, lo mismo que nosotros podemos hacer actualmente con nuestro móvil. Su intención, más que hacer una gran superprodución, era la de dar a conocer el invento a todo el mundo.</p>
+
+<p>Sin embargo en el siglo IV a.c. los chinos inventan una especie de linterna en la que en su interior llevaba una turbina que se accionaba con el viento o con un mecanismo dentado que giraba una persona. Así daba vueltas una pantalla en la que se encontraban dibujadas una serie de ilustraciones, animales plantas, estrellas, etc, que a su vez, una vela situada en el interior, con su luz traspasaba las ilustraciones que se proyectaba sobre paredes. Todo esto fue un anticipo del invento de los hermanos Lumìere.</p>
+
+<p>A lo largo del siglo XX el invento se fue perfeccionando con el sonido y el color. Ahora el cine es considerado un arte más como la pintura, la escultura o la literatura. Más precisamente es el séptimo arte.
+
+<p>Disfruta de esta web de cine clásico y recuerda nuestro lema: <span style="font-weight: bold">“Si eres amante del cine clásico este es tu sitio”</span></p>
 <?php include 'footer.php' ?>
