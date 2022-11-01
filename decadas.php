8f4e85bdc9d221eb069b8879f21918832eaaaa61
franjavi01 <fjsg0001@gmail.com>
2022-11-01 18:50:58 +0100

    ajustando el texto de cada card en decadas.php

--- decadas.php
+++ decadas.php
@@ -14,7 +14,8 @@
         <div class="col">
             <div class="card"><img class="card-img-top" src="<?php echo $row[2] ?>" alt="<?php echo $row[3] ?>">
                 <div class="card-body">
-                    <center><h3 class="card-text"><strong><?php echo $row[4] ?></strong></h3></center>
+                    <center><h2 class="card-text"><?php echo $row[4] ?></h2>
+                    <h2 class="card-text"><?php echo $row[5] ?></h2></center>
                 </div>
             </div>
         </div>
