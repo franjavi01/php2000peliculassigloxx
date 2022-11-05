43402c8e4bb5907972ae07a0d34a05278bdb176c
franjavi01 <fjsg0001@gmail.com>
2022-11-05 14:36:46 +0100

    implementando el modo oscuro

---
+++ main.js
@@ -0,0 +1,15 @@
+function noche() {
+  
+    document.getElementById("cuerpo").style.backgroundColor="black"; 
+      
+    document.getElementById("cuerpo").style.color="white";
+      
+    }
+    
+function dia() {
+      
+    document.getElementById("cuerpo").style.backgroundColor="white"; 
+      
+    document.getElementById("cuerpo").style.color="black";
+      
+    }
